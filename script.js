521d9c00dacb4fc958fa07247875ff05cab4e1a9
salikwani <120441251+salikwani@users.noreply.github.com>
2023-04-25 19:01:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function rem() {
+	var check = document.querySelector("#colorSelect").value;
+	
+	for(var i=1;i<=4;i++) {
+		var id = "c"+i;
+		if(document.querySelector(`#${id}`).value === check) {
+			document.querySelector(`#${id}`).remove();
+			break;
+		}
+	}
+}
